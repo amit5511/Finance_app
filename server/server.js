eccd0dfdc25dddb787a75eeacd8debf83f52cb2b
amit5511 <96247681+amit5511@users.noreply.github.com>
2023-03-26 19:24:36 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const app=require('./app');
+const path=require('path')
+
+//configure dot env
+const dotenv = require("dotenv");
+dotenv.config({ path: "server/configure/.env" });
+
+
+const cors=require('cors');
+app.use(cors({
+    credentials: true,
+    origin:['http://localhost:3000'],
+   
+   
+}));
+
+//mogodb connection
+const db_connection = require('./configure/dbConnection');
+const URL=process.env.db_URL;
+
+db_connection(URL);
+
+
+
+
+
+
+
+
+
+const PORT=process.env.PORT
+// server setup
+app.listen(PORT,()=>{
+    console.log(`Server is runing on PORT : ${PORT}`)
+})
